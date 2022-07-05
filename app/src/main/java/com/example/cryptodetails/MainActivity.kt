b57ba3d1af389b6df7029ae94af3e99bc2dbe429
Sanchit Monga <sanchitmonga22@gmail.com>
2022-07-05 12:52:36 -0400

    - Added the styled text input view

--- app/src/main/java/com/example/cryptodetails/MainActivity.kt
+++ app/src/main/java/com/example/cryptodetails/MainActivity.kt
@@ -21,7 +21,9 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment).navController
+        val navController =
+            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment).navController
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
@@ -36,27 +38,9 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-
-/**
- * {"code":"200000",
- * "data":[
- * {    "currency":"CSP",
- *      "name":"CSP",
- *      "fullName":"Caspian",
- *      "precision":8,
- *      "confirms":12,
- *      "contractAddress":"0xa6446d655a0c34bc4f05042ee88170d056cbaf45",
- *      "withdrawalMinSize":"10000",
- *      "withdrawalMinFee":"5000",
- *      "isWithdrawEnabled":true,
- *      "isDepositEnabled":true,
- *      "isMarginEnabled":false,
- *      "isDebitEnabled":false
- * },
- * {"currency":"LOKI","name":"OXEN","fullName":"Oxen","precision":8,"confirms":10,"contractAddress":"","withdrawalMinSize":"0.000000","withdrawalMinFee":"2.000000","isWithdrawEnabled":true,"isDepositEnabled":true,"isMarginEnabled":false,"isDebitEnabled":true},
- * {"currency":"NRG","name":"NRG","fullName":"Energi","precision":8,"confirms":5000,"contractAddress":"","withdrawalMinSize":"1","withdrawalMinFee":"1","isWithdrawEnabled":true,"isDepositEnabled":true,"isMarginEnabled":false,"isDebitEnabled":true},
- * {"currency":"FET","name":"FET","fullName":"Fetch.Ai","precision":8,"confirms":12,"contractAddress":"0x1d287cc25dad7ccaf76a26bc660c5f7c8e2a05bd","withdrawalMinSize":"100.000000","withdrawalMinFee":"46.000000","isWithdrawEnabled":true,"isDepositEnabled":true,"isMarginEnabled":false,"isDebitEnabled":false},
- * {"currency":"XMR","name":"XMR","fullName":"Monero","precision":8,"confirms":12,"contractAddress":"","withdrawalMinSize":"0.020000","withdrawalMinFee":"0.001000","isWithdrawEnabled":true,"isDepositEnabled":true,"isMarginEnabled":true,"isDebitEnabled":true},
- * {"currency":"RBTC","name":"RBTC","fullName":"RSK Smart Bitcoin","precision":8,"confirms":250,"contractAddress":"","withdrawalMinSize":"0.000200","withdrawalMinFee":"0.000150","isWithdrawEnabled":true,"isDepositEnabled":true,"isMarginEnabled":false,"isDebitEnabled":true},
- * {"currency":"RIF","name":"RIF","fullName":"RIF Token","precision":8,"confirms":250,"contractAddress":"","withdrawalMinSize":"20.000000","withdrawalMinFee":"10.000000","isWithdrawEnabled":true,"isDepositEnabled":true,"isMarginEnabled":false,"isDebitEnabled":true},
- */
+//        // Add your own reselected listener
+//        binding.navView.setOnItemReselectedListener { item ->
+//            // Pop everything up to the reselected item
+//            val reselectedDestinationId = item.itemId
+//            navController.popBackStack(reselectedDestinationId, inclusive = false)
+//        }
--- app/src/main/java/com/example/cryptodetails/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cryptodetails/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.cryptodetails.ui.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import com.example.cryptodetails.R
 import com.example.cryptodetails.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
@@ -17,20 +21,27 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        val textView: TextView = binding.searchBar
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+        }
+
+        binding.outlinedTextField.setEndIconOnClickListener {
+            val navController =
+                (requireActivity().supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment).navController
+            navController.navigate(R.id.navigation_my_account)
         }
         return root
     }
