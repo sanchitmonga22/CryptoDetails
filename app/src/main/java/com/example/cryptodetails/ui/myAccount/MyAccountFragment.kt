a198cabe3fa770b0053cd513ab05d5deb930c5c7
Sanchit Monga <sanchitmonga22@gmail.com>
2022-07-13 13:42:53 -0400

    - Added the permission handling capabilities and created an image view.

--- app/src/main/java/com/example/cryptodetails/ui/myAccount/MyAccountFragment.kt
+++ app/src/main/java/com/example/cryptodetails/ui/myAccount/MyAccountFragment.kt
@@ -1,10 +1,13 @@
 package com.example.cryptodetails.ui.myAccount
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -29,14 +32,80 @@ class MyAccountFragment : Fragment() {
         _binding = FragmentMyAccountBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
+
+        binding.profileImage.setOnClickListener {
+            showImagePickingOptionsDialog()
+            // select image from the gallery,
+            // click a new image with the camera and select that
+        }
+
         lifecycleScope.launchWhenCreated {
             myAccountViewModel.text.collectLatest {
-                textView.text = it
+
             }
         }
         return root
     }
+
+    private fun showImagePickingOptionsDialog() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(activity)
+        builder.setTitle("Select an image")
+        builder.setPositiveButton("Gallery") { dialog, which ->
+            if (!checkStoragePermission()) {
+                requestStoragePermission()
+            } else {
+                launchGalleryIntent()
+            }
+            dialog.dismiss()
+        }
+        builder.setNegativeButton("Camera") { dialog, which ->
+            if (!checkCameraPermission() || !checkStoragePermission()) {
+                requestCameraPermission()
+                requestStoragePermission()
+            } else {
+                launchCameraIntent()
+            }
+            dialog.dismiss()
+        }
+        builder.show()
+    }
+
+    private fun launchCameraIntent() {
+
+    }
+
+    private fun launchGalleryIntent() {
+
+    }
+
+    private fun requestStoragePermission() {
+        activity?.requestPermissions(
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            writeExternalPermissionRequestCode
+        )
+    }
+
+    companion object {
+        const val cameraPermissionRequestCode = 100
+        const val writeExternalPermissionRequestCode = 101
+    }
+
+    private fun requestCameraPermission() {
+        activity?.requestPermissions(
+            arrayOf(Manifest.permission.CAMERA), cameraPermissionRequestCode
+        )
+    }
+
+    private fun checkCameraPermission() = ContextCompat.checkSelfPermission(
+        requireActivity(),
+        Manifest.permission.CAMERA
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun checkStoragePermission() = ContextCompat.checkSelfPermission(
+        requireActivity(),
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    ) == PackageManager.PERMISSION_GRANTED
+
 
     override fun onDestroyView() {
         super.onDestroyView()
