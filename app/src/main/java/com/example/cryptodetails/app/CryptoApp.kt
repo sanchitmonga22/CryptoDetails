950efea5aaa93b4bd2331a43552ffc2245b906c7
Sanchit Monga <sanchitmonga22@gmail.com>
2022-07-18 12:12:34 -0400

    - Notifications practise 1.

--- app/src/main/java/com/example/cryptodetails/app/CryptoApp.kt
+++ app/src/main/java/com/example/cryptodetails/app/CryptoApp.kt
@@ -1,14 +1,43 @@
 package com.example.cryptodetails.app
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 
 class CryptoApp : Application() {
+
     companion object {
-//        const val
+        const val CHANNEL_1 = "Channel1"
+        const val CHANNEL_2 = "Channel2"
     }
 
     override fun onCreate() {
         super.onCreate()
+        createPNChannels()
+    }
 
+    private fun createPNChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val highImportanceChannel1 =
+                NotificationChannel(
+                    CHANNEL_1,
+                    "HIGH IMPORTANCE CHANNEL 1",
+                    NotificationManager.IMPORTANCE_HIGH
+                )
+            highImportanceChannel1.description = "This notification is of HIGH importance"
+
+
+            val lowImportanceNotificationChannel2 =
+                NotificationChannel(
+                    CHANNEL_2,
+                    "LOW IMPORTANCE CHANNEL 1",
+                    NotificationManager.IMPORTANCE_LOW
+                )
+            lowImportanceNotificationChannel2.description = "This notification is of low importance"
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(highImportanceChannel1)
+            manager.createNotificationChannel(lowImportanceNotificationChannel2)
+        }
     }
 }
--- app/src/main/java/com/example/cryptodetails/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/cryptodetails/ui/notifications/NotificationsFragment.kt
@@ -4,37 +4,62 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
+import com.example.cryptodetails.R
+import com.example.cryptodetails.app.CryptoApp
 import com.example.cryptodetails.databinding.FragmentNotificationsBinding
-import kotlinx.coroutines.flow.collectLatest
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
-
     private val binding get() = _binding!!
+    private lateinit var notificationManager: NotificationManagerCompat
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val notificationsViewModel = ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        binding.viewModel = notificationsViewModel
+        binding.lifecycleOwner = this@NotificationsFragment
 
-        val textView: TextView = binding.textNotifications
-        lifecycleScope.launchWhenCreated {
-            notificationsViewModel.text.collectLatest {
-                textView.text = it
-            }
+        notificationManager = NotificationManagerCompat.from(requireContext())
+        setupNotificationChannels()
+
+        return binding.root
+    }
+
+    private fun setupNotificationChannels() {
+        binding.sendOnChannel1.setOnClickListener {
+            val notificationChannel1 =
+                NotificationCompat.Builder(requireContext(), CryptoApp.CHANNEL_1)
+                    .setSmallIcon(R.drawable.error_loading_image)
+                    .setContentTitle(binding.notificationTitle.text)
+                    .setContentText(binding.notificationBody.text)
+                    .build()
+
+            notificationManager.notify(1, notificationChannel1)
+//                    .setCategory()
         }
-        return root
+
+        binding.sendOnChannel2.setOnClickListener {
+            val notificationChannel2 =
+                NotificationCompat.Builder(requireContext(), CryptoApp.CHANNEL_2)
+                    .setSmallIcon(R.drawable.error_loading_image)
+                    .setContentTitle(binding.notificationTitle.text)
+                    .setContentText(binding.notificationBody.text)
+                    .setCategory(NotificationCompat.CATEGORY_ERROR)
+                    .build()
+
+            notificationManager.notify(2, notificationChannel2)
+//                    .setCategory()
+        }
     }
 
     override fun onDestroyView() {
